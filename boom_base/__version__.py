c3bdddb2d34a8a0fa4725c133404652ad0e47324
samdychen <samdy.chen@visionwx.com>
2021-04-08 01:11:24 +0800

    【新增】增加api_calls apiCallRecord装饰器

--- boom_base/__version__.py
+++ boom_base/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'boom_base'
 __description__ = 'boom base package'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'VisionWX'
 __author_email__ = 'samdy.chen@visionwx.com'
 __url__ = 'https://www.visionwx.com'
