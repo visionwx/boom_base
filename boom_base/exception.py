30a0550d755fa2e0d11d712115c41a61ca46df32
samdychen <samdy.chen@visionwx.com>
2021-06-06 00:32:47 +0800

    【优化】自定义exception全部实现__str__接口

--- boom_base/exception.py
+++ boom_base/exception.py
@@ -2,49 +2,55 @@ class ParametersNotProvideException(Exception):
     def __init__(self, fieldName):
         self.fieldName = fieldName
     def __str__(self):
-        print("parameter:" + self.fieldName  + " not found")
+        return "parameter:" + self.fieldName  + " not found"
 
 class VideoNotExistException(Exception):
     def __init__(self, videoId):
         self.videoId = videoId
     def __str__(self):
-        print("videoId:" + self.videoId  + " not found")
+        return "videoId:" + self.videoId  + " not found"
 
 class CommentNotExistException(Exception):
     def __init__(self, commentId):
         self.commentId = commentId
     def __str__(self):
-        print("commentId:" + self.commentId  + " not found")
+        return "commentId:" + self.commentId  + " not found"
 
 class VideoInboxNotExistException(Exception):
     def __init__(self, videoInboxId):
         self.videoInboxId = videoInboxId
     def __str__(self):
-        print("videoInboxId:" + self.videoInboxId  + " not found")
+        return "videoInboxId:" + self.videoInboxId  + " not found"
 
 class DocumentNotExistException(Exception):
-    pass
+    def __str__(self):
+        return "DocumentNotExistException"
 
 class CollectionRequireFieldNotProvideException(Exception):
     def __init__(self, field):
         self.field = field
     def __str__(self):
-        print("Field:" + self.field  + " not provide")
+        return "Field:" + self.field  + " not provide"
 
 # 令牌超时
 class TokenExpireException(Exception):
-    pass
+    def __str__(self):
+        return "TokenExpireException"
 
 # 刷新令牌不存在
 class RefreshTokenNotExistException(Exception):
-    pass
+    def __str__(self):
+        return "RefreshTokenNotExistException"
 
 # 未提供令牌
 class TokenNotProvideException(Exception):
-    pass
+    def __str__(self):
+        return "RefreshTokenNotExistException"
 
 class PhoneCodeVerifyFailedException(Exception):
-    pass
+    def __str__(self):
+        return "PhoneCodeVerifyFailedException"
 
 class SendSmsCodeFailedException(Exception):
-    pass
+    def __str__(self):
+        return "SendSmsCodeFailedException"
